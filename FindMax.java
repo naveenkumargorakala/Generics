7609bd5cd070b870ce13f0b11b785abd171e377e
naveenkumargorakala <nicenaveengorakala@gmail.com>
2023-03-31 11:17:13 +0530

    Refactor: Find max of Three types dataTypes

--- FindMax.java
+++ FindMax.java
@@ -2,8 +2,8 @@ package com.bridgelabz.generics;
 
 import java.util.Scanner;
 
-public class FindMax {
-    public String maxOfThree(String a, String b, String c){
+public class FindMax<T extends Comparable<T>> {
+    public T maxOfThree(T a, T b, T c){
         if(a.compareTo(b)>0 && a.compareTo(c)>0)
             return a;
         else if(b.compareTo(c)>0)
@@ -11,16 +11,31 @@ public class FindMax {
         else
             return c;
     }
-
     public static void main(String[] args) {
-        FindMax findmax = new FindMax();
+        FindMax<Integer> findmax = new FindMax();
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter 3 Strings: ");
-        String a = input.next();
-        String b = input.next();
-        String c = input.next();
-        String max= findmax.maxOfThree(a,b,c);
+        System.out.println("Enter 3 Integers: ");
+        Integer a =input.nextInt();
+        Integer b = input.nextInt();
+        Integer c = input.nextInt();
+        Integer max= findmax.maxOfThree(a,b,c);
         System.out.println("max of: "+a+","+b+","+c+","+"is "+max);
+        System.out.println();
+        FindMax<Float> findmax1 = new FindMax();
+        System.out.println("Enter 3 Flaot Numbers: ");
+        float m =input.nextFloat();
+        float n = input.nextFloat();
+        float o = input.nextFloat();
+        float max1= findmax1.maxOfThree(m,n,o);
+        System.out.println("max of: "+m+","+n+","+o+","+"is "+max1);
+        System.out.println();
+        FindMax<String> findmax2 = new FindMax();
+        System.out.println("Enter 3 Strings: ");
+        String x =input.next();
+        String y = input.next();
+        String z = input.next();
+        String max2= findmax2.maxOfThree(x,y,z);
+        System.out.println("max of: "+x+","+z+","+c+","+"is "+max2);
 
     }
 }
