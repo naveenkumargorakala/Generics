844671bc5a60a3c24e43fda0586ab04622738410
naveenkumargorakala <nicenaveengorakala@gmail.com>
2023-03-31 10:46:52 +0530

    Added: Find max of Three Strings

--- FindMax.java
+++ FindMax.java
@@ -3,7 +3,7 @@ package com.bridgelabz.generics;
 import java.util.Scanner;
 
 public class FindMax {
-    public float maxOfThree(Float a, Float b, Float c){
+    public String maxOfThree(String a, String b, String c){
         if(a.compareTo(b)>0 && a.compareTo(c)>0)
             return a;
         else if(b.compareTo(c)>0)
@@ -15,11 +15,11 @@ public class FindMax {
     public static void main(String[] args) {
         FindMax findmax = new FindMax();
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter 3 Numbers: ");
-        float a = input.nextFloat();
-        float b = input.nextFloat();
-        float c = input.nextFloat();
-        float max= findmax.maxOfThree(a,b,c);
+        System.out.println("Enter 3 Strings: ");
+        String a = input.next();
+        String b = input.next();
+        String c = input.next();
+        String max= findmax.maxOfThree(a,b,c);
         System.out.println("max of: "+a+","+b+","+c+","+"is "+max);
 
     }
