a6bfe88f65e178a0fb4f4c76cb4d4d1b1a5ddd66
naveenkumargorakala <nicenaveengorakala@gmail.com>
2023-03-31 10:41:30 +0530

    Added: Find max of Three Float Numbers

--- FindMax.java
+++ FindMax.java
@@ -3,7 +3,7 @@ package com.bridgelabz.generics;
 import java.util.Scanner;
 
 public class FindMax {
-    public Integer maxOfThree(Integer a,Integer b,Integer c){
+    public float maxOfThree(Float a, Float b, Float c){
         if(a.compareTo(b)>0 && a.compareTo(c)>0)
             return a;
         else if(b.compareTo(c)>0)
@@ -16,11 +16,11 @@ public class FindMax {
         FindMax findmax = new FindMax();
         Scanner input = new Scanner(System.in);
         System.out.println("Enter 3 Numbers: ");
-        Integer a = input.nextInt();
-        Integer b = input.nextInt();
-        Integer c = input.nextInt();
-        Integer max=findmax.maxOfThree(a,b,c);
-        System.out.println("max of"+a+","+b+","+c+","+": "+max);
+        float a = input.nextFloat();
+        float b = input.nextFloat();
+        float c = input.nextFloat();
+        float max= findmax.maxOfThree(a,b,c);
+        System.out.println("max of: "+a+","+b+","+c+","+"is "+max);
 
     }
 }
