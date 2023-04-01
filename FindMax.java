d7c168b4292f453c9dc822eb022dd4169f6b34d6
naveenkumargorakala <nicenaveengorakala@gmail.com>
2023-04-01 10:41:35 +0530

    Add: Refactor the code with arrays

--- FindMax.java
+++ FindMax.java
@@ -1,51 +1,47 @@
 package com.bridgelabz.generics;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class FindMax<T extends Comparable<T>> {
+
     T a;
     T b;
     T c;
-    public FindMax(T a, T b, T c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-    public T maxOfThree(T a, T b, T c){
-        if(a.compareTo(b)>0 && a.compareTo(c)>0)
-            return a;
-        else if(b.compareTo(c)>0)
-            return b;
-        else
-            return c;
+//    public FindMax(T a, T b, T c) {
+//        this.a = a;
+//        this.b = b;
+//        this.c = c;
+//    }
+    public T testMax(T[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].compareTo(array[j]) > 0) {
+                    T temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+
+            }
+        }
+        return array[array.length - 1];
     }
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter 3 Integers: ");
-        Integer a =input.nextInt();
-        Integer b = input.nextInt();
-        Integer c = input.nextInt();
-        FindMax<Integer> findmax = new FindMax(a,b,c);
-        Integer max= findmax.maxOfThree(a,b,c);
-        System.out.println("max of: "+a+","+b+","+c+","+"is "+max);
+        FindMax<Integer> findmax = new FindMax();
+        Integer[] arr=new Integer[]{12,34,565,76};
+        Integer max= findmax.testMax(arr);
+        System.out.println("max of array is "+max);
         System.out.println();
 
-        System.out.println("Enter 3 Flaot Numbers: ");
-        float m =input.nextFloat();
-        float n = input.nextFloat();
-        float o = input.nextFloat();
-        FindMax<Float> findmax1 = new FindMax(m,n,o);
-        float max1= findmax1.maxOfThree(m,n,o);
-        System.out.println("max of: "+m+","+n+","+o+","+"is "+max1);
+        FindMax<Float> findmax1 = new FindMax();
+        Float[] arr1=new Float[]{23.f,4.f,5.f,43.f};
+        float max1= findmax1.testMax(arr1);
+        System.out.println("max of array is "+max1);
         System.out.println();
 
-        System.out.println("Enter 3 Strings: ");
-        String x =input.next();
-        String y = input.next();
-        String z = input.next();
-        FindMax<String> findmax2 = new FindMax(x,y,z);
-        String max2= findmax2.maxOfThree(x,y,z);
-        System.out.println("max of: "+x+","+z+","+c+","+"is "+max2);
-
+        FindMax<String> findmax2 = new FindMax();
+        String[] str= new String[]{"dffd","fd","Ff"};
+        String max2= findmax2.testMax(str);
+        System.out.println("max of array is "+max2);
     }
+
 }
